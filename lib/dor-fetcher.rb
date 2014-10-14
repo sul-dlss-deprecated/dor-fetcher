64f6c59baeb5b8a2a6364844355cf33a40823818
Carrick Rogers <carrickr@stanford.edu>
2014-10-14 14:07:33 -0700

    Service URL can now be configured.

--- lib/dor-fetcher.rb
+++ lib/dor-fetcher.rb
@@ -1,53 +1,58 @@
 require 'net/http'
 
-class DorFetcher
+module DorFetcher
+  
+  class Client
     
-    #TODO:  Make this load via a config file
-    @@fetcher_service_url = 'http://127.0.0.1:3000'
     @@supported_params = [:first_modified, :last_modified]
     @@count_only_param = "?rows=0"
     
+    
+    def initialize(service_url)
+      #TODO: Check for a well formed URL and a 200 from the destination before just accepting this
+      @service_url = service_url
+    end
     #options :count_only, :first_modified, :last_modified
-    def self.get_collection(collection, params = {})
+    def get_collection(collection, params = {})
       return query_api('collection', collection, params)
     end
     
-    def self.get_count_for_collection(collection, params = {})
+    def get_count_for_collection(collection, params = {})
       return query_api('collection', collection, add_count_only_param(params))
     end
   
-    def self.list_all_collections
+    def list_all_collections
       return query_api('collection', '', {})
     end
     
-    def self.total_collection_count
+    def total_collection_count
       return query_api('collection', '', {:count_only=>true})
     end
   
     #options :count_only, :first_modified, :last_modified
-    def self.get_apo(apo, params= {})
+    def get_apo(apo, params= {})
       return query_api('apo', apo, params)
     end
     
-    def self.get_count_for_apo(apo, params={})
+    def get_count_for_apo(apo, params={})
       return query_api('apo', apo, add_count_only_param(params))
     end
   
-    def self.list_all_apos
+    def list_all_apos
       return query_api('apo', '', {})
     end
     
-    def self.total_apo_count
+    def total_apo_count
       return query_api('apo', '', {:count_only=>true})
     end
     
     
-    def self.query_api(base_url, druid, params)
-      url = "#{@@fetcher_service_url}/#{base_url}/#{druid}#{add_params(params)}"
+    def query_api(base, druid, params)
+      url = "#{@service_url}/#{base}/#{druid}#{add_params(params)}"
       begin
         resp = Net::HTTP.get_response(URI.parse(url))
       rescue
-        raise 'Connection Error' 
+        raise 'Connection Error with url #{url}' 
       end
       
       return resp.body.to_i if params[:count_only] == true
@@ -55,7 +60,7 @@ class DorFetcher
       
     end
     
-    def self.add_params(params)
+    def add_params(params)
       args_string = ""
       
       #Handle Count Only
@@ -67,10 +72,11 @@ class DorFetcher
       return args_string
     end
     
-    def self.add_count_only_param(params)
+    def add_count_only_param(params)
       params.store(:count_only, true)
       return params
     end
+  end
     
 end
 
