f7f54e213269a66ba9e88a84ad539870a5e55e2b
Laney McGlohon <laneymcg@stanford.edu>
2014-10-21 11:09:31 -0700

    Added travis and coveralls and rake functionality

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ Bundler.setup
 require 'dor-fetcher'
 require 'json'
 require 'vcr'
+require 'coveralls'
+Coveralls.wear!
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
